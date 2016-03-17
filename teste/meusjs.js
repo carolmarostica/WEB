597517fb349e20c2cb88796d79e220714ceb236c
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-03-17 10:07:46 -0300

    Added files via upload

---
+++ teste/meusjs.js
@@ -0,0 +1,14 @@
+$( document). ready ( function () {
+	$("# fa ") . click ( function () {
+		$(". textof ") . fadeIn () ;
+	}) ;
+	$("# ff ") . click ( function () {
+		$(". textof ") . fadeOut () ;
+	}) ;
+	$("# ca ") . click ( function () {
+		$(". textoc ") . fadeIn () ;
+	}) ;
+	$("# cf ") . click ( function () {
+		$(". textoc ") . fadeOut () ;
+	}) ;
+}) ;
