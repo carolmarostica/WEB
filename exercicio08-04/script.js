b940c8f3fc6088dacc3a2f62bb67ba16e769f05a
Carolina Silva Marostica <carolcefet99@gmail.com>
2016-04-12 15:12:24 -0300

    Added files via upload

--- exercicio08-04/script.js
+++ exercicio08-04/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(e){
 	$("#dialog-confirm").hide();
 	$(".menuPrincipal a").click(function(e){
 		e.preventDefault();
-		var href=$(this).attr('href');
+		var href = $(this).attr('href');
 		$(".conteudo").load(href + ".conteudo");
 	});
 });
@@ -15,6 +15,46 @@ function dialogo(){
 		modal:true,
 		buttons:{
 			"Confirma": function(){
+				//VERIFICAÇÃO DO EMAIL//				
+				var e= document.querySelector("#email").value;
+				var TamanhoEmail = e.length;
+				var email1=false;
+				var email2=false;
+					for (var i=0; i<=TamanhoEmail; i++) {
+						if (e[i] == '@' ){	
+							var email=document.querySelector("#email").value.split('@');
+							if((email[0].length)>=3){
+								email1 =true;
+							}
+							if((email[1].length)>=4){
+								email2=true;
+							}			
+						}
+					}
+
+				//VERIFICAÇÃO DO NOME//
+				var n= document.querySelector("#nome").value;
+				var TamanhoNome = n.length;
+				var nome1=false;
+				var nome2=false;
+					for (var j=0; j<=TamanhoNome; j++) {
+						if (n[j] == ' ' ){	
+							var nome=document.querySelector("#nome").value.split(' ');
+							if((nome[0].length)>=3){
+								var nome1 =true;
+							}
+							if((nome[1].length)>=4){
+								var nome2=true;
+							}
+						
+						}
+					}
+				if(email1==true && email2==true && nome1==true && nome2==true){
+					alert("Dados enviados com sucesso!");
+				}else{
+					alert("Dados inseridos errado!");
+				}
+
 				$(this).dialog("close");
 			},
 			Cancelar: function(){
@@ -22,18 +62,5 @@ function dialogo(){
 			}
 		}
 	});
-	var e = document.getElementById("#email").value;
-	var TamanhoEmail = e.length;			
-	for (var i=0; i<=TamanhoEmail; i++) {
-		if (e[i] == '@' ){	
-			em = true;
-			alert("email ok!");
-		}
-	}
-	if (em==false){
-		$(".EmailErrado").show();
-		document.getElementById("idemail").value = " ";	
-		alert("email aff!");
-		}
-	}
+	
 };
